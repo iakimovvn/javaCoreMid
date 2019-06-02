3a6b9be8580c40ee84ee2c88dbd73821791d447f
Vladimir Iakimov <yakimovvn@bk.ru>
2019-06-03 00:14:36 +0300

    Написал главный класс MainClass

--- src/ru/yakimovvn/MainClass.java
+++ src/ru/yakimovvn/MainClass.java
@@ -2,6 +2,11 @@ package ru.yakimovvn;
 
 public class MainClass {
     public static void main(String[] args) {
+        Team team = new Team("Под фсфальт укладчики");
+        team.teamInfo();
+
+        Course course = new Course(new double[]{0.2,0.7,1.2,1.0,1.8,1.6,2.0,1.9,2.4,2.1,2.5});
+        course.passingDistance(team);
 
     }
 }
