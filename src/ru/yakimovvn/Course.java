127243740875d77d2520ea4b0d397eab0a4c5585
Vladimir Iakimov <yakimovvn@bk.ru>
2019-06-03 00:13:58 +0300

    Написал класс Course

--- src/ru/yakimovvn/Course.java
+++ src/ru/yakimovvn/Course.java
@@ -1,4 +1,27 @@
 package ru.yakimovvn;
 
 public class Course {
+    private double [] distance;
+
+    public  Course(double [] distance){
+        this.distance = distance;
+    }
+
+    private boolean isObstaclePassed(double maxJump, double obstacle){
+        return maxJump>=obstacle;
+    }
+
+    public void passingDistance(Team team) {
+        output:
+        for (int i = 0; i <team.getPlayers().length; i++) {
+            for (int j = 0; j <distance.length; j++) {
+                if(!isObstaclePassed(team.getPlayers()[i].getMaxJump(),distance[j])){
+                    break output;
+                }
+                team.pastDistancePlayers(team.getPlayers()[i]);
+            }
+        }
+    }
+
+
 }
