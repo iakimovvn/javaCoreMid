f9c8be797bf026fafda3c52e55087da64d0e2ac3
Vladimir Iakimov <yakimovvn@bk.ru>
2019-06-03 09:24:07 +0300

    Арфография асфальт.

--- src/ru/yakimovvn/MainClass.java
+++ src/ru/yakimovvn/MainClass.java
@@ -2,7 +2,7 @@ package ru.yakimovvn;
 
 public class MainClass {
     public static void main(String[] args) {
-        Team team = new Team("Под aсфальт укладчики");
+        Team team = new Team("Под асфальт укладчики");
         team.teamInfo();
 
         Course course = new Course(new double[]{0.2,0.7,1.2,1.0,1.8,1.6,2.0,1.9,2.4,2.1,2.5});
