b98ede899881d62d186b6a502a28703869e22143
Vladimir Iakimov <yakimovvn@bk.ru>
2019-06-04 00:22:27 +0300

    Отредактировал главный класс в соответствии с заданием.

--- src/ru/yakimovvn/MainClass.java
+++ src/ru/yakimovvn/MainClass.java
@@ -7,6 +7,6 @@ public class MainClass {
 
         Course course = new Course(new double[]{0.2,0.7,1.2,1.0,1.8,1.6,2.0,1.9,2.4,2.1,2.5});
         course.passingDistance(team);
-
+        team.showResults();
     }
 }
