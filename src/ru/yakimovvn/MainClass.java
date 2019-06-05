e1522f760de83c2529f3dd79d1ef096f98972cd8
Vladimir Iakimov <yakimovvn@bk.ru>
2019-06-05 10:29:40 +0300

    Сделал главный класс программы как требовалось в задании.

--- src/ru/yakimovvn/MainClass.java
+++ src/ru/yakimovvn/MainClass.java
@@ -1,11 +1,15 @@
 package ru.yakimovvn;
 
+import ru.yakimovvn.ObstacleCourse.*;
+import ru.yakimovvn.Tenderers.*;
+
+
+
 public class MainClass {
     public static void main(String[] args) {
-        Team team = new Team("Под асфальт укладчики");
-        team.teamInfo();
-
-        Course course = new Course(new double[]{0.2,0.7,1.2,1.0,1.8,1.6,2.0,1.9,2.4,2.1,2.5});
+        Team team = new Team("Под асфальт укладчики",new Human("Владимир"),
+                new Cat("Барсик"), new Dog("Жучка"),new Human("Сергей"));
+        Course course = new Course(new Wall(20),new Water(50),new Cross(2000));
         course.passingDistance(team);
         team.showResults();
     }
