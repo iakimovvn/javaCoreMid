a36303ccdafbd2c24b2927a8fe304a0578c08276
Vladimir Iakimov <yakimovvn@bk.ru>
2019-06-08 16:42:33 +0300

    создал метод isNumber(), проверяющий, является ли строка числом, применил его при генерации ошибки MyArrayDateException

--- src/LessonException/MainClass.java
+++ src/LessonException/MainClass.java
@@ -4,17 +4,22 @@ import LessonException.Exceptions.*;
 
 public class MainClass {
 
+    private static boolean isNumber (String str){
+        boolean res = true;
+        for(char c : str.toCharArray()){
+            if(!Character.isDigit(c)) res = false;
+        }
+        return res;
+    }
+
     private static int sumElementsOfArray(String [][] arrayOfNumbers) throws MyArraySizeException, MyArrayDataException {
         int sumElements = 0;
 
         if(arrayOfNumbers.length!=4 || arrayOfNumbers[0].length!=4) throw new MyArraySizeException();
         for (int i = 0; i <arrayOfNumbers.length ; i++) {
             for (int j = 0; j <arrayOfNumbers[i].length ; j++) {
-                try {
-                    sumElements +=Integer.parseInt(arrayOfNumbers[i][j]);
-                }catch(NumberFormatException e){
-                    throw new MyArrayDataException(i,j);
-                }
+                if(!isNumber(arrayOfNumbers[i][j])) throw new MyArrayDataException(i,j);
+                sumElements +=Integer.parseInt(arrayOfNumbers[i][j]);
             }
         }
         return sumElements;
@@ -27,7 +32,7 @@ public class MainClass {
                     {"2","3","4","5"},
                     {"6","3","2","5"},
                     {"2","4","4","1"},
-                    {"4","1","4","5"}}
+                    {"4","1","1","5"}}
                     )
             );
         } catch (MyArraySizeException es){
