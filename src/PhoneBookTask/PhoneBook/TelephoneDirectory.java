b170e910cd3d8a008ff044181686f38cf552f96a
Vladimir Iakimov <yakimovvn@bk.ru>
2019-06-11 23:26:11 +0300

    Определился с архитектурой приложения PhoneBook

---
+++ src/PhoneBookTask/PhoneBook/TelephoneDirectory.java
@@ -0,0 +1,4 @@
+package PhoneBookTask.PhoneBook;
+
+public class TelephoneDirectory {
+}
---
+++ src/PhoneBookTask/PhoneBookMain.java
@@ -0,0 +1,7 @@
+package PhoneBookTask;
+
+public class PhoneBookMain {
+    public static void main(String[] args) {
+
+    }
+}
