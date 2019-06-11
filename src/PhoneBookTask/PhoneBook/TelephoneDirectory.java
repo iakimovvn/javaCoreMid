4d1b89360cee0f031aaab05c79c32a1453579dbb
Vladimir Iakimov <yakimovvn@bk.ru>
2019-06-12 00:40:16 +0300

    Написад класс TelephoneDirectory. В нем методы add() и get().

--- src/PhoneBookTask/PhoneBook/TelephoneDirectory.java
+++ src/PhoneBookTask/PhoneBook/TelephoneDirectory.java
@@ -1,4 +1,31 @@
 package PhoneBookTask.PhoneBook;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
 public class TelephoneDirectory {
+    private HashMap<String, Set<Long>> phoneTable;
+
+    public TelephoneDirectory() {
+        phoneTable = new HashMap<>();
+    }
+
+    public void add(String surName,Long ... phones){
+        surName = surName.toUpperCase();
+        if(phoneTable.containsKey(surName)){
+            for (Long phone : phones) {
+                phoneTable.get(surName).add(phone);
+            }
+        }
+        else{
+            phoneTable.put(surName,new HashSet<>(Set.of(phones)));
+        }
+    }
+
+    public String get(String surName){
+        surName = surName.toUpperCase();
+        return (phoneTable.containsKey(surName))?surName+": "+ phoneTable.get(surName)
+                : "Контакта "+surName+", не обнаружено!";
+    }
 }
