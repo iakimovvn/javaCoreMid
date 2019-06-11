2ff7443f18613956471f9b08c7884b5481f99e70
Vladimir Iakimov <yakimovvn@bk.ru>
2019-06-12 00:40:33 +0300

    Написал главный класс.

--- src/PhoneBookTask/PhoneBookMain.java
+++ src/PhoneBookTask/PhoneBookMain.java
@@ -1,7 +1,14 @@
 package PhoneBookTask;
 
+import PhoneBookTask.PhoneBook.TelephoneDirectory;
+
 public class PhoneBookMain {
     public static void main(String[] args) {
-
+        TelephoneDirectory phoneBook = new TelephoneDirectory();
+        phoneBook.add("Vladimir",89625957309L,423434324234L);
+        System.out.println(phoneBook.get("vladimir"));
+        System.out.println(phoneBook.get("Ivan"));
+        phoneBook.add("vladimir",12321232L);
+        System.out.println(phoneBook.get("vladimir"));
     }
 }
