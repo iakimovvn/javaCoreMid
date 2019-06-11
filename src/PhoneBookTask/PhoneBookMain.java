4359919f98c74bf7bdd5f607022bb57b4a06fbb1
Vladimir Iakimov <yakimovvn@bk.ru>
2019-06-12 00:46:13 +0300

    Дописал ещё одного пользователя.

--- src/PhoneBookTask/PhoneBookMain.java
+++ src/PhoneBookTask/PhoneBookMain.java
@@ -6,8 +6,10 @@ public class PhoneBookMain {
     public static void main(String[] args) {
         TelephoneDirectory phoneBook = new TelephoneDirectory();
         phoneBook.add("Vladimir",89625957309L,423434324234L);
+        phoneBook.add("Irina",2323243543L);
         System.out.println(phoneBook.get("vladimir"));
         System.out.println(phoneBook.get("Ivan"));
+        System.out.println(phoneBook.get("irina"));
         phoneBook.add("vladimir",12321232L);
         System.out.println(phoneBook.get("vladimir"));
     }
