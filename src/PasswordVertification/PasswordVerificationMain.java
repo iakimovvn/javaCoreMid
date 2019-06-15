5701e8413cab19e0d9160451446cd03dd5ee5d34
Vladimir Iakimov <yakimovvn@bk.ru>
2019-06-15 13:58:35 +0300

    Дополнил логику выводом сообщения.

--- src/PasswordVertification/PasswordVerificationMain.java
+++ src/PasswordVertification/PasswordVerificationMain.java
@@ -7,31 +7,38 @@ import java.util.regex.Pattern;
 
 public class PasswordVerificationMain {
 
-    private static boolean isPasswordValid(String password){
+    private static String isPasswordValid(String password){
 
-        boolean res;
+        boolean isValid;
+        String res;
 
-        Pattern patternSymbols = Pattern.compile("^[A-z,0-9,_,\\-,<,>,\\.,\\,]{8,20}$");
+        Pattern patternSymbols = Pattern.compile("^[A-z,0-9 ,_,\\-,<,>,\\.,\\,]{8,20}$");
         Matcher matcher = patternSymbols.matcher(password);
-        res = matcher.matches();
+        isValid = matcher.matches();
+        res = "Недопустимые символы/размер";
 
-        if(res){
+        if(isValid){
             Pattern patternIsNumber = Pattern.compile(".*\\d+.*");
             matcher = patternIsNumber.matcher(password);
-            res = matcher.matches();
+            isValid = matcher.matches();
+            res = "Не обнаружено чисел";
+
         }
-        if(res){
+        if(isValid){
             Pattern patternIsOneBig = Pattern.compile(".*[A-Z]+.*");
             matcher = patternIsOneBig.matcher(password);
-            res = matcher.matches();
+            isValid = matcher.matches();
+            res = "Не обнаружено заглавных букв";
 
         }
-        if(res){
+        if(isValid){
             Pattern patternIsOneBig = Pattern.compile(".*[_,\\-,<,>,\\.,\\,]+.*");
             matcher = patternIsOneBig.matcher(password);
-            res = matcher.matches();
-
+            isValid = matcher.matches();
+            res = "Не обнаружено специальных символов.";
         }
+        if (isValid) res = password+"прошёл вертификацию";
+        else res += " Пароль не подходит.";
 
 
         return res;
@@ -41,15 +48,14 @@ public class PasswordVerificationMain {
 
         Scanner scanner = new Scanner(System.in);
         String inputPassword;
+        System.out.println("Пароль.\nОт 8 до 20 символовю Можно использовать только Латинские буквы, цыфры,\n" +
+                "и символы (<),(>),(.),(-),(_), должна быть хотябы одна Заглавная буква,\n" +
+                "одна цыфра и один символ\nВведите пароль:");
 
-        System.out.println("Введите пароль:");
-        inputPassword = scanner.nextLine();
-
-        System.out.println((isPasswordValid(inputPassword))?inputPassword+"\t-прошёл вертификацию."
-                :inputPassword+"\t-не подходит");
-
-
+        //while (true) {
+            inputPassword = scanner.nextLine();
+            System.out.println(isPasswordValid(inputPassword));
+        //}
         scanner.close();
-
     }
 }
