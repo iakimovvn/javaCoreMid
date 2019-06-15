4918b97a87de52191811b42f72b2a5a9ec725e18
Vladimir Iakimov <yakimovvn@bk.ru>
2019-06-15 13:58:54 +0300

    Нписал код идентичный созданному на уроке.

---
+++ src/ChatWindowJavaFX/sample/Controller.java
@@ -0,0 +1,22 @@
+package ChatWindowJavaFX.sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class Controller {
+
+    @FXML
+    private TextArea textArea;
+
+    @FXML
+    private TextField textField;
+
+    public void sendMsg(){
+        textArea.appendText(textField.getText()+"\n");
+        textField.clear();
+        textField.requestFocus();
+    }
+
+
+}
